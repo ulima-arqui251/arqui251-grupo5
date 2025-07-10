8574cbacbbf0d33c171a5e94042bb560ca86ca43
Jmat12 <nchampa23@gmail.com>
2025-07-10 16:11:06 -0500

    a

---
+++ 0/0.9/Máquinas_virtuales-Champa/Código/ubuntu-back.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+
+app = Flask(__name__)
+CORS(app)  
+
+latest_data = {'Ubuntu'}
+
+@app.route('/send', methods=['POST'])
+def receive_data():
+    global latest_data
+    latest_data = request.json
+    print("Se recibio la siguiente Data:", latest_data)
+    return {'status': 'Data received'}
+
+@app.route('/receive', methods=['GET'])
+def send_data():
+    return jsonify(latest_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
